b5b811aa6d22da5c232b4754fcfb9ed5baebd232
DinkeyKing <lanyivilmos@gmail.com>
2024-02-16 23:13:28 +0100

    progress

--- Assets/Entities/Car/Car.cs
+++ Assets/Entities/Car/Car.cs
@@ -1,5 +1,5 @@
 using System.Collections.Generic;
-using Unity.VisualScripting;
+using System.Drawing;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
@@ -11,6 +11,14 @@ public class Car : MonoBehaviour
     public float maxFollowDistance = 1.0f;
     public float minFollowDistance = 0.5f;
 
+    public float rayOffsetLength = 0.2f;
+
+    public float boxRightOffsetLength = 0.25f;
+    public float boxUpOffsetLength = 0.5f;
+    public Vector2 boxSize = new(0.5f,  0.5f);
+    public float maxYieldTime = 3.0f;
+    public float yieldTime = 0.0f;
+
     public float speed = 0.0f;
     public float defaultSpeed = 0.0f;
     public Vector3 velocity;
@@ -18,7 +26,7 @@ public class Car : MonoBehaviour
     public AIState state;
 
     private bool positionReached;
-    private Vector3 direction;
+    private Vector3 moveDirection;
     private Vector3 targetPosition;
     private Vector3 currentOffset;
     private Vector3Int targetTilePosition;
@@ -30,12 +38,85 @@ public class Car : MonoBehaviour
     public enum AIState
     {
         STOP,
-        DRIVE
+        DRIVE,
+        YIELD
+    }
+
+
+    // Debug draws
+    private void OnDrawGizmos()
+    {
+        // Right car detection box
+        Vector3 offset = transform.up * boxUpOffsetLength + transform.right * boxRightOffsetLength;
+        Vector3 overlapPosition = transform.position + offset;
+
+        Gizmos.DrawWireCube(overlapPosition, boxSize);
+    }
+
+
+    private bool IsOnIntersection()
+    {
+        Vector3Int currentTilePosition = roadTilemap.WorldToCell(transform.position);
+
+        List<Vector3Int> neighborTiles = new();
+
+
+        // NORTH
+        Vector3Int northTilePosition = currentTilePosition + Vector3Int.up;
+        var northTile = roadTilemap.GetTile(northTilePosition);
+        if (northTile != null) neighborTiles.Add(northTilePosition);
+
+        // EAST
+        Vector3Int eastTilePosition = currentTilePosition + Vector3Int.right;
+        var eastTile = roadTilemap.GetTile(eastTilePosition);
+        if (eastTile != null) neighborTiles.Add(eastTilePosition);
+
+        // SOUTH
+        Vector3Int southTilePosition = currentTilePosition + Vector3Int.down;
+        var southTile = roadTilemap.GetTile(southTilePosition);
+        if (southTile != null) neighborTiles.Add(southTilePosition);
+
+        // WEST
+        Vector3Int westTilePosition = currentTilePosition + Vector3Int.left;
+        var westTile = roadTilemap.GetTile(westTilePosition);
+        if (westTile != null) neighborTiles.Add(westTilePosition);
+
+        if (neighborTiles.Count > 2) return true;
+
+        return false;
+    }
+
+
+    private bool PriorityToTheRight()
+    {
+        Vector3 offset = transform.up * boxUpOffsetLength + transform.right * boxRightOffsetLength;
+        Vector3 overlapPosition = transform.position + offset;
+
+        var collider = Physics2D.OverlapBox(overlapPosition, boxSize, 0.0f);
+
+
+        if (collider != null)
+        {
+            Car otherCar = collider.GetComponent<Car>();
+
+            if (otherCar != null)
+            {
+                // We need to know if the detected car is moving towards the intersection/us.
+                Vector3 fromOtherToSelf = (transform.position - collider.transform.position).normalized;
+
+                if (Vector3.Dot(fromOtherToSelf, collider.transform.up) > 0.0f && otherCar.speed > 0.0 && IsOnIntersection())
+                {
+                    return true;
+                }
+            }
+        }
+
+        return false;
     }
 
 
     // Selects a random tile position from the given position's neighbouring tiles. If dead end, returns the given previous tile's position. 
-    public Vector3Int DecideNextTilePosition(Vector3Int currentTilePosition, Vector3Int previousTilePosition)
+    private Vector3Int DecideNextTilePosition(Vector3Int currentTilePosition, Vector3Int previousTilePosition)
     {
         TileBase previousTile = roadTilemap.GetTile(previousTilePosition);
 
@@ -65,7 +146,7 @@ public class Car : MonoBehaviour
 
         if (possibleForwardTiles.Count == 0) return previousTilePosition;  // Dead end
 
-        // Pick a random direction from the possible options and return it
+        // Pick a random direction from the possible options and return it.
         int randomIndex = Random.Range(0, possibleForwardTiles.Count);
 
         return possibleForwardTiles[randomIndex];
@@ -74,9 +155,11 @@ public class Car : MonoBehaviour
 
     private void SetSpeed()
     {
-        var hit = Physics2D.Raycast(transform.position, direction, maxFollowDistance);
+        Vector3 rayOffset = moveDirection * rayOffsetLength;  // Make sure ray doesn't hit own collider.
+        Vector3 rayOrigin = transform.position + rayOffset;
+        var hit = Physics2D.Raycast(rayOrigin, moveDirection, maxFollowDistance);
         
-        Debug.DrawRay(transform.position, direction * maxFollowDistance);
+        Debug.DrawRay(rayOrigin, moveDirection * maxFollowDistance);
         
         if (hit.collider != null)
         {
@@ -84,12 +167,11 @@ public class Car : MonoBehaviour
 
             if (carInFront != null)
             {
-                if (carInFront.speed <= speed && Vector3.Dot(velocity, carInFront.velocity) > 0.0f)
+                if (Vector3.Dot(moveDirection, carInFront.moveDirection) > 0.0f)
                 {
-                    speed = carInFront.speed;
-
-                    if (hit.distance < minFollowDistance) speed = 0.0f;  // If too close to the car in front, stop to make som space and avoid intersection.
-
+                    if (hit.distance < minFollowDistance) speed = carInFront.speed * 0.5f;  // If too close to the car in front, slow down to make some space and try to avoid intersections.
+                    else speed = Mathf.Min(carInFront.speed, defaultSpeed);  // Pickup the followed car's speed only if it's smaller than the default speed.
+                    
                     return;
                 }
             }
@@ -98,25 +180,11 @@ public class Car : MonoBehaviour
         speed = defaultSpeed;
     }
 
-    private void Awake()
+
+    // The car moves around on the roads, picking random directions.
+    private void Drive()
     {
-        defaultSpeed = minSpeed + UnityEngine.Random.value * (maxSpeed - minSpeed);
-
-        if (roadTilemap == null)  // The tilemap reference must be assigned before instantiated.
-        {
-            Destroy(gameObject);
-        }
-
-        state = AIState.DRIVE;
-
-        Vector3Int currentTilePosition = roadTilemap.WorldToCell(transform.position);
-        previousTilePosition = currentTilePosition;
-        positionReached = true;
-    }
-    
-    void Update()
-    {
-       if (positionReached)
+        if (positionReached)
         {
             Vector3Int currentTilePosition = roadTilemap.WorldToCell(transform.position);
 
@@ -130,12 +198,12 @@ public class Car : MonoBehaviour
             if (targetTilePosition - currentTilePosition == Vector3Int.down) laneOffset = Vector3.left * laneOffsetLength;
             if (targetTilePosition - currentTilePosition == Vector3Int.left) laneOffset = Vector3.up * laneOffsetLength;
 
-            if(laneOffset != currentOffset)
+            if (laneOffset != currentOffset)
             {
                 transform.position += laneOffset;
                 currentOffset = laneOffset;
             }
-            
+
 
             targetPosition = roadTilemap.GetCellCenterWorld(targetTilePosition);
             targetPosition = new Vector3(targetPosition.x, targetPosition.y, transform.position.z);  // The z position will be constant during all movement.
@@ -147,11 +215,14 @@ public class Car : MonoBehaviour
         }
 
 
-        direction = (targetPosition - transform.position).normalized;
-        transform.up = direction;  // Set the car rotation, so it looks towards the target point.
+        moveDirection = (targetPosition - transform.position).normalized;
+
+        // Set the car rotation, so it looks towards the target point.
+        float angle = Vector2.SignedAngle(Vector2.up, moveDirection); // Calculate the angle from the object's 'up' to the direction
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward); // Apply rotation
 
         SetSpeed();
-        velocity = speed * direction;
+        velocity = speed * moveDirection;
         Vector3 motion = Time.deltaTime * velocity;
 
         transform.position += motion;
@@ -164,4 +235,71 @@ public class Car : MonoBehaviour
             Vector3Int currentTilePosition = roadTilemap.WorldToCell(transform.position);
         }
     }
+
+
+    private void Awake()
+    {
+        if (roadTilemap == null)  // The tilemap reference must be assigned before instantiated.
+        {
+            Destroy(gameObject);
+        }
+
+        defaultSpeed = minSpeed + UnityEngine.Random.value * (maxSpeed - minSpeed);
+
+        state = AIState.DRIVE;
+
+        Vector3Int currentTilePosition = roadTilemap.WorldToCell(transform.position);
+        previousTilePosition = currentTilePosition;
+        positionReached = true;
+    }
+    
+
+    void Update()
+    {
+        // State transitions
+        switch (state)
+        {
+            case AIState.STOP:
+                // The traffic light will set and change this state
+                break;
+
+            case AIState.DRIVE:
+
+                if (PriorityToTheRight())
+                {
+                    yieldTime = maxYieldTime;
+                    state = AIState.YIELD;
+                }
+                break;
+
+            case AIState.YIELD:
+
+                if (!PriorityToTheRight() || yieldTime <= 0.0f) state = AIState.DRIVE;
+
+                break;
+        }
+
+        // State behaviours
+        switch (state)
+        {
+            case AIState.STOP:
+                speed = 0.0f;
+                velocity = Vector3.zero;
+                break;
+
+            case AIState.YIELD:
+                speed = 0.0f;
+                velocity = Vector3.zero;
+
+                yieldTime -= Time.deltaTime;
+                break;
+
+            case AIState.DRIVE:
+                Drive();
+                break;
+        }
+    }
+
+
+
 }
--- Assets/Entities/TrafficLight/TrafficLight.cs
+++ Assets/Entities/TrafficLight/TrafficLight.cs
@@ -1,4 +1,3 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,46 +6,79 @@ public class TrafficLight : MonoBehaviour
 
     public float passTime = 5.0f;
     public float stopTime = 5.0f;
-
-    public bool is_in_pass_state = true;
+    public State state;
+    public Vector2 boxSize = new(1.0f, 1.0f);
 
     private SpriteRenderer spriteRenderer;
     private float remainingTime = 0.0f;
 
+    new private Collider2D collider;
+
+    private List<Car> carsToWakeUp = new List<Car>();
+
+    public enum State
+    {
+        PASS,
+        STOP
+    }
+
+
+    // The car will be stopped only when entering the collider, if the lamp turns red and the car is already in the trigger area, it won't be stopped.
+    private void OnTriggerEnter2D(Collider2D other_collider)
+    {
+        if (state == State.STOP)
+        {
+            Car car = other_collider.GetComponent<Car>();
+            if (car != null)
+            {
+                car.state = Car.AIState.STOP;
+
+                carsToWakeUp.Add(car);
+            }
+        }
+    }
+
+
+    // Notify stopped cars that were stopped, so they can move again
+    private void WakeAllCars()
+    {
+        foreach (Car car in carsToWakeUp)
+        {
+            car.state = Car.AIState.DRIVE;
+        }
+        carsToWakeUp.Clear();
+    }
+
+    // Sets the new state and applies changes
+    private void SetState(State new_state)
+    {
+        state = new_state;
+
+        switch (state)
+        {
+            case State.PASS:
+                remainingTime = passTime;
+                spriteRenderer.color = Color.green;
+
+                // The cars that were stopped need to be informed here that they can pass
+                WakeAllCars();
+
+                break;
+
+            case State.STOP:
+                remainingTime = stopTime;
+                spriteRenderer.color = Color.red;
+                break;
+        }
+    }
 
 
     private void Awake()
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
+        collider = GetComponent<Collider2D>();
 
-        remainingTime = passTime;
-        UpdateColor();
-    }
-
-
-    private void ResetTimer()
-    {
-        if (is_in_pass_state)
-        {
-            remainingTime = passTime;
-        }
-        else
-        {
-            remainingTime = stopTime;
-        }
-    }
-
-
-    private void UpdateColor()
-    {
-        if (is_in_pass_state)
-        {
-            spriteRenderer.color = Color.green;
-        }
-        else
-        {
-            spriteRenderer.color = Color.red;
-        }
+        SetState(State.PASS);
     }
 
 
@@ -54,12 +86,18 @@ public class TrafficLight : MonoBehaviour
     {
         remainingTime -= Time.deltaTime;
 
-        if (remainingTime < 0.0f)
+        if (remainingTime <= 0.0f)
         {
-            is_in_pass_state = !is_in_pass_state;
+            switch (state)
+            {
+                case State.PASS:
+                    SetState(State.STOP);
+                    break;
 
-            UpdateColor();
-            ResetTimer();
+                case State.STOP:
+                    SetState(State.PASS);
+                    break;
+            }
         }
     }
 }
